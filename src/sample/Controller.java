7ba669d49bc37dbf961161c763be8c2a725f00d4
daydromer79 <florisvermeir77@gmail.com>
2019-09-09 12:02:47 +0100

    java fx knop en scherm toegevoegd en java code hier heb je geen numpy nodig dat maakt het eenvoudiger alles rechtstreeks in java doen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static javafx.application.Application.launch;
 
@@ -10,5 +21,31 @@ public class Controller {
     public static void main(String[] args) {
         launch(args);
     }
-    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+   // System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+    @FXML
+    private Button button;
+    @FXML
+    private ImageView currentFrame;
+
+    @FXML
+    protected void startCamera(ActionEvent event) {
+
+        private VideoCapture capture = new VideoCapture();
+        Runnable frameGrabber = new Runnable(){};
+        this.timer = Executors.newSingleThreadScheduledExecutor();
+            this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+
+            if (this.capture.isOpened()){
+                System.out.println(" Capture opened");
+            }
+
+            //this.capture.release(); if you want to release the capture  before the destructor is called
+        Mat frame = new Mat();
+            this.capture.read(frame);
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", frame, buffer);
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,14 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("camera view.fxml"));
+        BorderPane root = (BorderPane) loader.load();
+        FXController controller = loader.getController();
+        primaryStage.setTitle("Webcam Toner");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }

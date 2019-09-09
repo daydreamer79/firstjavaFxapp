9c4d6450fd6ee4892d16948ade129d33ec93bd30
daydromer79 <florisvermeir77@gmail.com>
2019-09-09 12:10:42 +0100

    dit zou moeten werken

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -12,6 +14,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
+import java.io.ByteArrayInputStream;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +24,8 @@ public class Controller {
     public static void main(String[] args) {
         launch(args);
     }
-   // System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+   private VideoCapture capture = new VideoCapture();
 
     @FXML
     private Button button;
@@ -32,20 +36,31 @@ public class Controller {
     protected void startCamera(ActionEvent event) {
 
         private VideoCapture capture = new VideoCapture();
-        Runnable frameGrabber = new Runnable(){};
-        this.timer = Executors.newSingleThreadScheduledExecutor();
-            this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+        Runnable frameGrabber = new Runnable() {};
+        this.timer =Executors.newSingleThreadScheduledExecutor();
+            this.timer.scheduleAtFixedRate(frameGrabber,0,33,TimeUnit.MILLISECONDS);
 
-            if (this.capture.isOpened()){
+            if(this.capture.isOpened())
+
+            {
                 System.out.println(" Capture opened");
             }
 
             //this.capture.release(); if you want to release the capture  before the destructor is called
-        Mat frame = new Mat();
+            Mat frame = new Mat();
             this.capture.read(frame);
-            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
-        MatOfByte buffer = new MatOfByte();
-        Imgcodecs.imencode(".png", frame, buffer);
+            Imgproc.cvtColor(frame,frame,Imgproc.COLOR_BGR2GRAY);
+            MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png",frame,buffer);
+        new Image(new ByteArrayInputStream(buffer.toArray()));
+            Image imageToShow = grabFrame();
+        }
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                currentFrame.setImage(imageToShow);
+            }
+        }
 
     }
 }
